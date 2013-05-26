e5707b3b403bbc67175e53c912e84e44268c602f
Gerson <gerson.scanapieco@gmail.com>
2013-05-26 13:24:34 -0300

    Changes for OAuth work

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -6,7 +6,9 @@ class GithubController < ApplicationController
   def index
     info = parse_url(params[:repositoryURL])
     github = Github.new :client_id => '942e65ee8b3ba57761ea', :client_secret => 'd464b55fbe4b80f280a255a10a1688658eacf34f'
-    #github.authorize_url :redirect_uri => 'http://localhost', :scope => 'repo'
+    auth = github.authorize_url , :scope => 'repo'
+    @token = github.get_token(auth)
+
     @branches = get_repo_branches(github,info[:repo_name],info[:owner] )
     if (params[:startDate]=="" and params[:endDate]=="")
       @commits_list = github.repos.commits.list info[:owner], info[:repo_name]    
