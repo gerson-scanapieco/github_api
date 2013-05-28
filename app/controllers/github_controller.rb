0cf61e61522b47faa63243641e0f10e4df9507dd
Gerson <gerson.scanapieco@gmail.com>
2013-05-28 15:12:13 -0300

    Bug fix. Now it shows the commits beetween dates correctly

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -27,7 +27,7 @@ class GithubController < ApplicationController
     return branches_info
   end
 
-  def fetch_commits_data(git_connection,repo_name,owner,start_date="",end_date="")
+  def fetch_commits_data(git_connection,repo_name,owner,start_date,end_date)
     commits_list={}
     branches = get_repo_branches(git_connection,repo_name,owner) 
     if (start_date=="" and end_date=="")
@@ -35,7 +35,7 @@ class GithubController < ApplicationController
         commits_list["#{branch_name}"]  = (git_connection.repos.commits.list owner, repo_name, :sha => branch_name)
       end
     else
-      branches.keys.each do |branch|
+      branches.keys.each do |branch_name|
         commits_list["#{branch_name}"]  = (git_connection.repos.commits.list owner,repo_name, start_date, end_date, :sha => branch_name)
       end
     end
